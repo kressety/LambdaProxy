02d78f81689c709241613338e5d5f17893374448
Zhu Ziqi <kressety@163.com>
2023-12-12 22:34:21 +0000

    Update module and dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module geekr-dev/openai-proxy
+module kressety/LambdaProxy
 
 go 1.17
+
+require github.com/aws/aws-lambda-go v1.42.0
--- main.go
+++ main.go
@@ -1,132 +1,84 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io"
-	"log"
+	"context"
+	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
-	"strconv"
 	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 var (
-	target string // 目标域名
-	port   int    // 代理端口
-	// httpProxy = "http://127.0.0.1:10809" // 本地代理地址和端口
+	target = os.Getenv("TARGET_URL")
 )
 
-func main() {
-	// 从命令行参数获取配置文件路径
-	flag.StringVar(&target, "domain", "https://api.openai.com", "The target domain to proxy.")
-	flag.IntVar(&port, "port", 9000, "The proxy port.")
-	flag.Parse()
-
-	// 打印配置信息
-	log.Println("Target domain: ", target)
-	log.Println("Proxy port: ", port)
-
-	http.HandleFunc("/", handleRequest)
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// 过滤无效URL
-	_, err := url.Parse(r.URL.String())
+	_, err := http.NewRequest(request.HTTPMethod, request.Path, nil)
 	if err != nil {
-		log.Println("Error parsing URL: ", err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
 	}
 
 	// 去掉环境前缀（针对腾讯云，如果包含的话，目前我只用到了test和release）
-	newPath := strings.Replace(r.URL.Path, "/release", "", 1)
-	newPath = strings.Replace(newPath, "/test", "", 1)
+	newPath := strings.Replace(request.Path, "/openai-go", "", 1)
+	newPath = strings.Replace(newPath, "/default", "", 1)
 
-	// 拼接目标URL（带上查询字符串，如果有的话）
-	// 如果请求中包含 X-Target-Host 头，则使用该头作为目标域名
-	// 优先级 header > args > default
-	var targetURL string
-	if r.Header.Get("X-Target-Host") != "" {
-		targetURL = "https://" + r.Header.Get("X-Target-Host") + newPath
-	} else {
-		targetURL = target + newPath
-	}
-	if r.URL.RawQuery != "" {
-		targetURL += "?" + r.URL.RawQuery
-	}
-
-	// 本地打印代理请求完整URL用于调试
-	if os.Getenv("ENV") == "local" {
-		fmt.Printf("Proxying request to: %s\n", targetURL)
-	}
+	// 拼接目标URL
+	targetURL := target + newPath
 
 	// 创建代理HTTP请求
-	proxyReq, err := http.NewRequest(r.Method, targetURL, r.Body)
+	proxyReq, err := http.NewRequest(request.HTTPMethod, targetURL, strings.NewReader(request.Body))
 	if err != nil {
-		log.Println("Error creating proxy request: ", err.Error())
-		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
-		return
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
 	}
 
 	// 将原始请求头复制到新请求中
-	for headerKey, headerValues := range r.Header {
+	for headerKey, headerValues := range request.Headers {
 		for _, headerValue := range headerValues {
-			proxyReq.Header.Add(headerKey, headerValue)
+			proxyReq.Header.Add(headerKey, string(headerValue))
 		}
 	}
 
-	// 默认超时时间设置为300s（应对长上下文）
+	// 默认超时时间设置为60s
 	client := &http.Client{
-		// Timeout: 300 * time.Second,  // 代理不干涉超时逻辑，由客户端自行设置
+		Timeout: 60 * time.Second,
 	}
 
-	// 支持本地测试通过代理请求
-	/*if os.Getenv("ENV") == "local" {
-		proxyURL, _ := url.Parse(httpProxy) // 本地HTTP代理配置
-		client.Transport = &http.Transport{
-			Proxy:           http.ProxyURL(proxyURL),
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-	}*/
-
-	// 发起代理请求
+	// 向 OpenAI 发起代理请求
 	resp, err := client.Do(proxyReq)
 	if err != nil {
-		log.Println("Error sending proxy request: ", err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
 	}
 	defer resp.Body.Close()
 
 	// 将响应头复制到代理响应头中
+	responseHeaders := make(map[string]string)
 	for key, values := range resp.Header {
+		var headerValue string
 		for _, value := range values {
-			w.Header().Add(key, value)
+			headerValue += value + ","
 		}
+		responseHeaders[key] = strings.TrimSuffix(headerValue, ",")
 	}
 
-	// 将响应状态码设置为原始响应状态码
-	w.WriteHeader(resp.StatusCode)
-
-	// 将响应实体写入到响应流中（支持流式响应）
-	buf := make([]byte, 1024)
-	for {
-		if n, err := resp.Body.Read(buf); err == io.EOF || n == 0 {
-			return
-		} else if err != nil {
-			log.Println("error while reading respbody: ", err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		} else {
-			if _, err = w.Write(buf[:n]); err != nil {
-				log.Println("error while writing resp: ", err.Error())
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.(http.Flusher).Flush()
-		}
+	// 读取响应实体到字节数组中
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
 	}
+
+	// 返回API Gateway响应
+	return events.APIGatewayProxyResponse{
+		StatusCode: resp.StatusCode,
+		Headers:    responseHeaders,
+		Body:       string(responseBody),
+	}, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
 }
